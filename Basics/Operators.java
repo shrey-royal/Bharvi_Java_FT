a9979cf8a2d93cb47a3d6dad25cf08bc98f3561f
Shrey K <srkkadia@gmail.com>
2023-10-30 10:13:08 +0530

    init

---
+++ Basics/Operators.java
@@ -0,0 +1,20 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 12;
+
+        // Arithmetic Operators
+        System.out.println("The value of a + b is " + (a + b) + ".");
+    }
+}
+/*
+7 types of operators:
+
+1. arithmetic -> +, -, *, /, %(modulus)
+2. relational -> <, >, <=, >=, ==, !=
+3. assignment -> =, +=, -=, *=, /=, %=
+4. logical -> &&, ||, !
+5. bitwise -> &, |, ^, ~, <<, >>, >>>
+6. ternary -> ?:
+7. increment/decrement -> ++, --
+*/
